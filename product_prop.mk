c2e08340ca20b055c6a2116756d170a5742451a8
Martin Liu <liumartin@google.com>
2020-03-10 17:05:58 +0800

    umi: Remove lmkd properties to enable new lmkd kill strategy
    With new strategy default values for lmkd properties should work
    and no property overrides are necessary.
    
    Bug: 143857475
    Test: lmkd_unit_test
    Change-Id: I52c8c93ad5adb5e741790939e381b7c0adae4261
    Signed-off-by: Akash Srivastava <akashniki@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -14,18 +14,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # LMKD
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.lmk.low=1001 \
-    ro.lmk.medium=800 \
-    ro.lmk.critical=0 \
-    ro.lmk.critical_upgrade=false \
-    ro.lmk.upgrade_pressure=100 \
-    ro.lmk.downgrade_pressure=100 \
-    ro.lmk.kill_heaviest_task=true \
-    ro.lmk.kill_timeout_ms=100 \
-    ro.lmk.use_minfree_levels=true \
-    ro.lmk.log_stats=true \
-    ro.lmk.swap_util_max=95 \
-    ro.lmk.kill_timeout_ms=100
+    ro.config.low_ram = false
 
 # Media
 PRODUCT_PRODUCT_PROPERTIES += \
