eb8146e6759ad079b5cc27151dd942dafcab09f1
LuK1337 <priv.luk@gmail.com>
2020-07-07 12:47:29 +0200

    cmi: Implement vendor.lineage.powershare@1.0 HAL
    Adapted for cmi

--- kona.mk
+++ kona.mk
@@ -146,6 +146,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.ims.xml
 
+# PowerShare
+PRODUCT_PACKAGES += \
+    lineage.powershare@1.0-service.xiaomi_kona
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.qcom.rc \
