55f192278d1f97043b081fca3084eac61bfc0fc5
Prasanta Kumar Sahu <prassa@codeaurora.org>
2020-07-22 20:59:02 +0530

    umi: Enable incremental FS feature
    Change-Id: I12c2a493c07c738ef962798aa8dcac5eda071f54
    Signed-off-by: Akash Srivastava <akashniki@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -8,6 +8,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=1
 
+# Incremental FS
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.incremental.enable=1
+
 # LMKD
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.lmk.low=1001 \
