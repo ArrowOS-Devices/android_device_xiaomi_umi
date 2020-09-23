003e57d0640a23d8be3459984ba94a721bb0c294
Akash Srivastava <akashniki@gmail.com>
2020-09-23 20:27:46 +0200

    umi: Add Dex props

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,5 +57,15 @@ TARGET_BOOTLOADER_BOARD_NAME := kona
 # Camera
 TARGET_USES_QTI_CAMERA_DEVICE := true
 
+# Dex
+ifeq ($(HOST_OS),linux)
+    ifneq ($(TARGET_BUILD_VARIANT),eng)
+        ifeq ($(WITH_DEXPREOPT),)
+            WITH_DEXPREOPT := true
+            WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+        endif
+    endif
+endif
+
 # VNDK
 BOARD_VNDK_VERSION := current
