2f71fc3fc51d85acf51e6930f63eacd8e9a907e3
Akash Srivastava <akashniki@gmail.com>
2020-09-24 09:36:30 +0200

    umi: Add needed system props

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,27 @@
 # Bluetooth
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.bluetooth.library_name=libbluetooth_qti.so
+
+# CNE
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.vendor.cne.feature=1
+
+# DPM
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.vendor.dpm.feature=9
+
+# IMS
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1
+
+# Radio
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.telephony.default_network=22,22
+
+# Netmgr
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.vendor.data.iwlan.enable=true \
+    ro.vendor.use_data_netmgrd=true \
+    persist.vendor.data.mode=concurrent
