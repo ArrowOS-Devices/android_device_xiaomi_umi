3ae30bbb5c7bcd375a1d426799b0dd5cd3478a2f
Akash Srivastava <akashniki@gmail.com>
2020-09-24 09:38:15 +0200

    umi: Inherit QTI componenets for PA

--- kona.mk
+++ kona.mk
@@ -95,6 +95,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.ims.xml
 
+# QTI common
+TARGET_COMMON_QTI_COMPONENTS := \
+    av \
+    bt \
+    perf \
+    telephony \
+    wfd
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
