10f822250f0a1591d612f27aafae5de4b681aa10
Akash Srivastava <akashniki@gmail.com>
2020-09-28 11:50:59 +0200

    cmi: Remove audio_policy_volume
    Volume issue was caused by compressed_offload which was broken, but that has been fixed now.

--- kona.mk
+++ kona.mk
@@ -51,8 +51,7 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_policy_engine_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_engine_configuration.xml \
-    $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_volumes.xml
+    $(LOCAL_PATH)/audio/audio_policy_engine_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_engine_configuration.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
