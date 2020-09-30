21d9bc9f0aa5030834173c95bdde0a5a8ec32940
Akash Srivastava <akashniki@gmail.com>
2020-09-30 19:34:58 +0200

    cmi: Build telephony packages

--- kona.mk
+++ kona.mk
@@ -153,3 +153,16 @@ TARGET_COMMON_QTI_COMPONENTS := \
 PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.recovery.qcom.rc
+
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
