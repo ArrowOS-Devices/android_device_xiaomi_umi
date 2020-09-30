67c8ebcbc8e07a2404f5e870ece3934a523e4be3
Akash Srivastava <akashniki@gmail.com>
2020-09-30 19:32:15 +0200

    cmi: Inhert telephony and aosp base

--- kona.mk
+++ kona.mk
@@ -14,6 +14,10 @@
 # limitations under the License.
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
