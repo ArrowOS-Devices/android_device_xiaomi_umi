25fa0f195bf0584da7a71562a9f0be6da6c4345b
Akash Srivastava <akashniki@gmail.com>
2020-10-02 00:05:00 +0200

    cmi: Update system props to MIUI 12.0.4

--- system_prop.mk
+++ system_prop.mk
@@ -8,6 +8,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.bluetooth.library_name=libbluetooth_qti.so \
     vendor.bluetooth.soc=cherokee
 
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
+
 # CNE
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.vendor.cne.feature=1
@@ -15,6 +19,12 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # DPM
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.vendor.dpm.feature=9
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.enable_hwc_vds=1
 
 # IMS
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -24,14 +34,35 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-    media.settings.xml=/system/etc/media_profiles_vendor.xml
+    media.settings.xml=/system/etc/media_profiles_vendor.xml \
+    vendor.mm.enable.qcom_parser=63963135
 
-# Radio
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.telephony.default_network=22,22
+# Memory optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.sys.fw.bservice_enable=true
+
+# Netflix
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.netflix.bsp_rev=Q855-16947-1
 
 # Netmgr
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.vendor.data.iwlan.enable=true \
     ro.vendor.use_data_netmgrd=true \
     persist.vendor.data.mode=concurrent
+
+# Radio
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.telephony.default_network=22,22
+
+# RCS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.rcs.supported=1
+
+# Sensors
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.sensors.enable.mag_filter=true
+
+# Vendor
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.va_aosp.support=1
