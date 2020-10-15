76735336a7f18c6fe2527a00b92de80e64c8f07c
Akash Srivastava <akashniki@gmail.com>
2020-10-15 13:27:09 +0200

    cmi: Add support for Google's Voice Recorder

--- product_prop.mk
+++ product_prop.mk
@@ -2,6 +2,11 @@
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.stagefright.omx_default_rank.sw-audio=16
 
+# Recorder
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.vendor.audio.fluence.voicerec=true \
+    persist.vendor.audio.fluence.audiorec=true
+
 # Telephony
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.telephony.default_network=22,22 \
