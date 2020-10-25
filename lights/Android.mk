fb9a622d450e6ca4b64bf40285f46b197d5d9c9e
Akash Srivastava <akashniki@gmail.com>
2020-10-25 21:37:15 +0100

    cmi: Remove depricated libhidltransport and libhwbinder

--- lights/Android.mk
+++ lights/Android.mk
@@ -13,7 +13,6 @@ LOCAL_SHARED_LIBRARIES := \
         libbase \
         liblog \
         libhidlbase \
-        libhidltransport \
         libhardware \
         libutils \
         android.hardware.light@2.0
