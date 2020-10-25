2117327503fc6717df9d54f4bbcb24b922adebd7
Akash Srivastava <akashniki@gmail.com>
2020-10-25 23:19:13 +0100

    cmi: Change build fingerprint to Pixel 5

--- cafex_cmi.mk
+++ cafex_cmi.mk
@@ -33,8 +33,11 @@ PRODUCT_MODEL := MI 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="Xiaomi/cmi/cmi:10/QKQ1.191117.002/V12.0.6.0.QJACNXM:user/release-keys" \
-    PRODUCT_NAME="cmi" \
-    TARGET_DEVICE="cmi"
+    PRIVATE_BUILD_DESC="redfin-user 11 RD1A.200810.020 6792687 release-keys" \
+
+BUILD_FINGERPRINT := google/redfin/redfin:11/RD1A.200810.020/6792687:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
