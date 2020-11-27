a8687e61366473ac0e922cc3aa1a8a9df8b03e6b
Akash Srivastava <akashniki@gmail.com>
2020-11-27 20:44:59 +0100

    umi: Build back powershare

--- kona.mk
+++ kona.mk
@@ -180,8 +180,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.ims.xml
 
 # PowerShare
-#PRODUCT_PACKAGES += \
-#    lineage.powershare@1.0-service.xiaomi_kona
+PRODUCT_PACKAGES += \
+    lineage.powershare@1.0-service.xiaomi_kona
 
 # QTI common
 TARGET_COMMON_QTI_COMPONENTS := \
